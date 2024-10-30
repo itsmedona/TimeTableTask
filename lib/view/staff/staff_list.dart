b96b6174b3d63b641008f45f3e2514e042be3b88
itsmedona <donashaji254@gmail.com>
2024-10-30 13:05:23 +0530

    screens created for course,staff, subject and timetable

---
+++ lib/view/staff/staff_list.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StaffList extends StatelessWidget {
+  const StaffList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:Text('StaffList'),),
+    );
+  }
+}
