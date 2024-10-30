8757b5bddc90db63055be37f2da84acc4e87d80d
itsmedona <donashaji254@gmail.com>
2024-10-30 13:05:36 +0530

    screens created for course,staff, subject and timetable

--- lib/view/timetable/timetable.dart
+++ lib/view/timetable/timetable.dart
@@ -6,6 +6,8 @@ class Timetable extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
+      body: Center(child: Text('Timetable'),),
       
     );
   }
