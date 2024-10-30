3fa8f14b1a56d73e8b4dab5a76e2f0fc6aa4d38c
itsmedona <donashaji254@gmail.com>
2024-10-30 13:03:14 +0530

    home screen created

---
+++ lib/widgets/homescreen_widget/homescreen_tile.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenTile extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final VoidCallback onTap;
+
+  HomeScreenTile({
+    required this.icon,
+    required this.label,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.blueAccent,
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, size: 50, color: Colors.white),
+            SizedBox(height: 10),
+            Text(
+              label,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
