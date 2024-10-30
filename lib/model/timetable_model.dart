1743052942857e22d9d6953e3489d443c3faa63b
itsmedona <donashaji254@gmail.com>
2024-10-30 13:04:34 +0530

    models created

---
+++ lib/model/timetable_model.dart
@@ -0,0 +1,18 @@
+/*class Timetable {
+  final String id;
+  final String courseId;
+  final String subjectId;
+  final String staffId;
+  final DateTime startTime;
+  final DateTime endTime;
+
+  Timetable({
+    required this.id,
+    required this.courseId,
+    required this.subjectId,
+    required this.staffId,
+    required this.startTime,
+    required this.endTime,
+  });
+}
+*/
