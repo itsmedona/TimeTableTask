ca4d3b00ee8177f77459a3510ad6b23ab77c6714
itsmedona <donashaji254@gmail.com>
2024-10-30 17:30:49 +0530

    screens created

--- lib/model/course_model.dart
+++ lib/model/course_model.dart
@@ -1,8 +1,9 @@
+import 'subject_model.dart';
+
 class Course {
   final String id;
   final String name;
-  final List<String> subjectIds;
-  
-  Course({required this.id, required this.name, required this.subjectIds});
+  final List<Subject> subjects; 
+  Course({required this.id, required this.name, this.subjects = const []});
 }
 
