ebc66f9a8a113991115c732c251c666406f2aa14
itsmedona <donashaji254@gmail.com>
2024-10-30 13:04:24 +0530

    models created

---
+++ lib/model/subject_model.dart
@@ -0,0 +1,8 @@
+class Subject {
+  final String id;
+  final String name;
+  final String courseId;
+  final String staffId;
+
+  Subject({required this.id, required this.name, required this.courseId, required this.staffId});
+}
